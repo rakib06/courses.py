9084a2ade3ce658785cf85bbcf9bf2a4fed2f8d5
Rakibul Hasan <rakibul.hasan@bracits.com>
2021-10-31 00:43:13 +0600

    linked list insert, traverse and remove

--- c-48-ds.py/linkedList.py
+++ c-48-ds.py/linkedList.py
@@ -21,5 +21,42 @@ class LinkedList:
             self.head = newNode
     
     # O(1)
-    def size(self)
+    def size(self):
         return self.size 
+    
+    # O(1)
+    def insertLast(self, data):
+        self.size += 1
+        newNode = Node(data)
+        
+        selectedNode = self.head
+        
+        while selectedNode is not None:
+            selectedNode = selectedNode.newNode
+            
+        selectedNode.nextNode = newNode
+    
+    def traverseList(self):
+        actualNode = self.head
+        
+        while actualNode is not None:
+            print("%d" % actualNode.data)
+            actualNode = actualNode.nextNode
+    
+    def remove(self, data):
+        if self.head is not None: #if  the node is empty 
+            return 
+        
+        currentNode = self.head
+        previousNode = None
+        
+        while currentNode != data:
+            previousNode = currentNode 
+            currentNode = currentNode.nextNode
+            
+        if previousNode is None:
+            self.head = currentNode.newNode # if the element is the first node 
+        else:
+            previousNode.nextNode = currentNode.nextNode
+    
+    
